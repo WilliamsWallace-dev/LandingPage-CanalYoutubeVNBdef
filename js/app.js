2e1660b6cb7d7271b93fc89c9dcf110502ebb1aa
Williams Wallace <wwallace.dev@gmail.com>
2023-01-31 17:13:54 -0300

    animation of skills section has been finished

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 
-let textSkills = document.querySelector(".skills-container .text-container");
-console.log(textSkills)
-let skillOne = document.querySelector(".skillOne");
-let skillTwo = document.querySelector(".skillTwo");
-let skillThree =  document.querySelector(".skillThree");
-let skillFour =  document.querySelector(".skillFour");
+// let textSkills = document.querySelector(".skills-container .text-container");
+// console.log(textSkills)
+// let skillOne = document.querySelector(".skillOne").addEventListener(click, skillActive);
+// let skillTwo = document.querySelector(".skillTwo");
+// let skillThree =  document.querySelector(".skillThree");
+// let skillFour =  document.querySelector(".skillFour");
+
+// let skillActive = ()=>{
+//     skillOne.
+// }
